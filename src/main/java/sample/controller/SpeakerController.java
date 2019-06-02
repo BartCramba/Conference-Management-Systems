82dd9de576c3b759dfaa4756ac55497336e2f8ae
Alexandra <alecrisan22@gmail.com>
2019-06-02 18:54:57 +0300

    submit proposal done

--- src/main/java/sample/controller/SpeakerController.java
+++ src/main/java/sample/controller/SpeakerController.java
@@ -1,14 +1,78 @@
 package sample.controller;
 
+import com.sun.deploy.util.StringUtils;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
+import javafx.stage.Stage;
+import sample.domain.Proposal;
+import sample.domain.Topic;
+import sample.repository.ProposalRepositoryImpl;
+import sample.repository.UserRepositoryImpl;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.*;
 
 public class SpeakerController implements Initializable {
 
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("sample");
+    EntityManager em = emf.createEntityManager();
+    ProposalRepositoryImpl repo = new ProposalRepositoryImpl(em);
+
+    @FXML
+    private Button submitProposalButton;
+
+    @FXML
+    private TextField proposalName;
+
+    @FXML
+    private TextField keywords;
+
+    @FXML
+    private TextField topics;
+
+    @FXML
+    private TextField authors;
+
     public void handleSubmitProposalButton(ActionEvent actionEvent) {
+        Proposal p = new Proposal();
+
+        p.setName(proposalName.getText());
+        String[] k = keywords.getText().split(",");
+        p.setKeywords(Arrays.asList(k));
+        String[] t = topics.getText().split(",");
+
+        List<Topic> l = new ArrayList<Topic>();
+        for(String s : t) {
+            Topic topic = new Topic();
+            topic.setName(s);
+            l.add(topic);
+        }
+        p.setTopics(l);
+
+        System.out.println(p);
+
+        repo.save(p);
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setHeaderText("SUCCESS");
+        alert.setContentText("Paper submitted!");
+        alert.show();
+
     }
 
     @Override
---
+++ src/main/java/sample/repository/ProposalRepositoryImpl.java
@@ -0,0 +1,66 @@
+package sample.repository;
+
+
+import sample.domain.Proposal;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceUnit;
+import java.util.List;
+
+public class ProposalRepositoryImpl implements BaseRepository<Proposal> {
+
+    @PersistenceUnit(unitName = "sample")
+    private EntityManager em;
+
+    public ProposalRepositoryImpl(EntityManager em) {
+        this.em = em;
+    }
+
+    public ProposalRepositoryImpl() {}
+
+    @Override
+    public Proposal findById(int id) {
+        return em.find(Proposal.class, id);
+    }
+    /*
+    @Override
+    public List<User> getAll() {
+        TypedQuery<User> q = em.createQuery("SELECT user from Users user")
+    }
+    */
+
+    @Override
+    public Proposal save(Proposal object) {
+        try{
+            em.getTransaction().begin();
+            em.persist(object);
+            em.getTransaction().commit();
+        }finally{
+            if (em.getTransaction().isActive()){
+                em.getTransaction().rollback();
+            }
+        }
+        return object;
+    }
+
+    @Override
+    public void delete(Proposal object) {
+        if (em.contains(object)) {
+            em.remove(object);
+        } else {
+            em.merge(object);
+        }
+    }
+
+
+//    public Proposal getByUsername(String email){
+//        List<Proposal> result = em.createNamedQuery(Proposal.PROPOSAL, Proposal.class)
+//                .setParameter(Proposal.EMAIL, email)
+//                .getResultList();
+//        if(result.isEmpty()){
+//            return new Proposal();
+//        }
+//        return result.get(0);
+//    }
+}
+
